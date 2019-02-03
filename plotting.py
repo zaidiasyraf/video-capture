04b7b91c0808f9563b27eac0d5f6c9c320540f81
zaidiasyraf <zaidi.asyraf@yahoo.com>
2019-02-03 13:22:18 +0800

    first commit

---
+++ plotting.py
@@ -0,0 +1,23 @@
+from capture import df
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool, ColumnDataSource
+
+df["Start_String"]=df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_String"]=df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+
+cds=ColumnDataSource(df)
+
+p=figure(x_axis_type="datetime", height =100, width=500, sizing_mode="scale_width", title="Motion Graph")
+p.yaxis.minor_tick_line_color=None
+p.ygrid[0].ticker.desired_num_ticks=1
+
+
+
+hover=HoverTool(tooltips=[("Start: ","@Start_String"),("End: ","@End_String")])
+p.add_tools(hover)
+
+q=p.quad(left="Start",right="End",bottom=0,top=1,color="green", source=cds)
+
+output_file("Graph.html")
+show(p)
